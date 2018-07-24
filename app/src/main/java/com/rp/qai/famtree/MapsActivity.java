3aa8d722aded74b33195ccdc84c0398fa1ab1972
QaiWalker <16014753@myrp.edu.sg>
2018-07-25 00:56:29 +0800

    I guess it works??

--- app/src/main/java/com/rp/qai/famtree/MapsActivity.java
+++ app/src/main/java/com/rp/qai/famtree/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.rp.qai.famtree;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
@@ -11,6 +13,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,10 +25,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapsActivity extends AppCompatActivity {
 
     private GoogleMap map;
     Member data;
+    EditText etGetLocation;
+    Button btnGetLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,14 @@ public class MapsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maps);
         Intent i = getIntent();
         data = (Member)i.getSerializableExtra("data");
-
-
+        etGetLocation = findViewById(R.id.etGetLocation);
+        btnGetLocation = findViewById(R.id.btnGetCoord);
 
         FragmentManager fm = getSupportFragmentManager();
         SupportMapFragment mapFragment = (SupportMapFragment)
                 fm.findFragmentById(R.id.map);
 
+        etGetLocation.setText(data.getAddress());
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -62,9 +74,65 @@ public class MapsActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
                     return;
                 }
+
+            }
+        });
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GetCoordinates().execute(etGetLocation.getText().toString().replace(" ","+"));
             }
         });
     }
+    private class GetCoordinates extends AsyncTask<String,Void,String> {
+        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait....");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            String response;
+            try{
+                String address = strings[0];
+                HttpDataHandler http = new HttpDataHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s",address);
+                response = http.getHTTPData(url);
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+
+                String lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lat").toString();
+                String lng = ((JSONArray)jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lng").toString();
+
+                LatLng current = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 17));
+
+                if(dialog.isShowing())
+                    dialog.dismiss();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
